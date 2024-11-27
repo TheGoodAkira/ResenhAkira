0d757f941451f70fc183aa961edaa3c05d53badd
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2024-11-27 16:00:12 -0300

    Atualização

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
+    'Literatura',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
+    'Cinema',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'Literatura',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Cinema',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
