64e8fcabfa8f855bc840e6e23472cc29d6f4a93b
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2024-11-27 16:14:24 -0300

    Atualizar

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Literatura',
-    'O que é Python?',
+    'Os Sete Maridos De Evelyn Hugo',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Cinema',
-    'Qual a capital da França?',
+    'Nimona',
     'A capital da França é Paris'
 )
 
 criaCartao(
     'Literatura',
-    'O que é uma função?',
+    'Eu e Esse Meu Coração',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
     'Cinema',
-    'Como se diz oi em Inglês?',
+    'A Culpa é das Estrelas',
     'Oi em ingles é HI (RAI)'
 )
