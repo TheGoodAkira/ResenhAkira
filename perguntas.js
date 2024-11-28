e918d377d95d9d585b4f7ad7773f48ca0592bba5
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2024-11-28 20:32:31 -0300

    ATUALIZAR

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Literatura',
     'Os Sete Maridos De Evelyn Hugo',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Cinema',
-    'Nimona',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Literatura',
-    'Eu e Esse Meu Coração',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    '"Os Sete Maridos de Evelyn Hugo", de Taylor Jenkins Reid, é uma fascinante obra que narra a vida da icônica atriz de Hollywood, Evelyn Hugo, que decide contar sua história a uma jovem jornalista, Monique Grant. Ao longo da narrativa, Evelyn revela os altos e baixos de sua vida, incluindo os sete casamentos que teve, cada um refletindo suas ambições, desejos e os desafios que enfrentou na busca pela fama e amor em um mundo dominado por preconceitos. Através de uma prosa envolvente e personagens complexos, Reid explora temas como identidade, amor e sacrifício, enquanto desvenda os segredos de uma mulher que, apesar da superficialidade da indústria do cinema, busca autenticidade e conexão. O livro é uma reflexão poderosa sobre o que significa ser mulher em um mundo implacável e a luta por ser verdadeiramente vista.'
 )
 
 criaCartao(
     'Cinema',
     'A Culpa é das Estrelas',
-    'Oi em ingles é HI (RAI)'
+    '"A Culpa é das Estrelas", baseado no romance de John Green, é um tocante filme que narra a história de Hazel Grace Lancaster, uma adolescente que luta contra o câncer e que, ao frequentar um grupo de apoio, conhece Augustus Waters, um carismático sobrevivente da doença. A relação entre os dois se desenvolve de forma intensa e comovente, enquanto eles compartilham experiências, medos e sonhos, desafiando as limitações impostas pela doença. O filme aborda temas como amor, perda e a busca por significado em meio ao sofrimento, equilibrando momentos de humor e tristeza. Com atuações marcantes e diálogos profundos, "A Culpa é das Estrelas" é uma reflexão sensível sobre a fragilidade da vida e o poder das conexões humanas, tocando o coração do público de maneira memorável.'
+)
+
+criaCartao(
+    'Literatura',
+    'Eu e Esse Meu Coração',
+    '"Eu e Esse Meu Coração", de Carina Rissi, é um romance contemporâneo que envolve a jovem e sonhadora Sofia, que se vê em meio a um dilema emocional ao ter que escolher entre seguir seu coração ou as expectativas da sociedade. A história é narrada com leveza e humor, enquanto Sofia enfrenta desafios em sua vida amorosa, especialmente com o enigmático e atraente Miguel. A autora aborda temas como autodescoberta, amor verdadeiro e a luta interna entre o que se deseja e o que se espera de nós. Com personagens bem desenvolvidos e diálogos envolventes, Rissi entrega uma narrativa que explora a busca pela felicidade e a importância de ouvir o próprio coração, tornando a leitura cativante e reflexiva.'
+)
+
+criaCartao(
+    'Cinema',
+    'Nimona',
+    '"Nimona" é uma animação vibrante e cheia de ação, baseada na webcomic de ND Stevenson. A história segue Nimona, uma jovem shapeshifter que se torna aliada de Ballister Blackheart, um cavaleiro injustamente acusado de um crime. Juntos, eles desafiam a ordem estabelecida em um reino futurista que mistura elementos medievais com tecnologia avançada. O filme aborda temas como amizade, aceitação e a luta contra a opressão, enquanto Nimona, com sua personalidade rebelde e humor contagiante, questiona as normas sociais e o que significa ser um vilão ou um herói. Com uma animação estilizada e uma trilha sonora envolvente, "Nimona" é uma história emocionante que mistura aventura e reflexão, cativando tanto jovens quanto adultos.'
 )
