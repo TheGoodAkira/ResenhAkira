1f1f5e454f19dd42e75489e7a969625d4fe7dabf
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2024-11-29 18:02:13 -0300

    Update

--- perguntas.js
+++ perguntas.js
@@ -1,40 +1,40 @@
 criaCartao(
     'Literatura',
     'Os Sete Maridos De Evelyn Hugo',
-    'Os Sete Maridos de Evelyn Hugo, de Taylor Jenkins Reid, é uma fascinante obra que narra a vida da icônica atriz de Hollywood, Evelyn Hugo, que decide contar sua história a uma jovem jornalista, Monique Grant. Ao longo da narrativa, Evelyn revela os altos e baixos de sua vida, incluindo os sete casamentos que teve, cada um refletindo suas ambições, desejos e os desafios que enfrentou na busca pela fama e amor em um mundo dominado por preconceitos. Através de uma prosa envolvente e personagens complexos, Reid explora temas como identidade, amor e sacrifício, enquanto desvenda os segredos de uma mulher que, apesar da superficialidade da indústria do cinema, busca autenticidade e conexão. O livro é uma reflexão poderosa sobre o que significa ser mulher em um mundo implacável e a luta por ser verdadeiramente vista.'
+    '"Os Sete Maridos de Evelyn Hugo", de Taylor Jenkins Reid, narra a vida da icônica atriz de Hollywood, Evelyn Hugo, que revela à jornalista Monique Grant os altos e baixos de seus sete casamentos, explorando temas de identidade, amor e autenticidade em um mundo dominado por preconceitos.'
 )
 
 criaCartao(
     'Cinema',
     'A Culpa é das Estrelas',
-    'A Culpa é das Estrelas, baseado no romance de John Green, é um tocante filme que narra a história de Hazel Grace Lancaster, uma adolescente que luta contra o câncer e que, ao frequentar um grupo de apoio, conhece Augustus Waters, um carismático sobrevivente da doença. A relação entre os dois se desenvolve de forma intensa e comovente, enquanto eles compartilham experiências, medos e sonhos, desafiando as limitações impostas pela doença. O filme aborda temas como amor, perda e a busca por significado em meio ao sofrimento, equilibrando momentos de humor e tristeza. Com atuações marcantes e diálogos profundos, "A Culpa é das Estrelas" é uma reflexão sensível sobre a fragilidade da vida e o poder das conexões humanas, tocando o coração do público de maneira memorável.'
+    '"A Culpa é das Estrelas", baseado no romance de John Green, é um tocante filme que segue Hazel Grace Lancaster, uma adolescente com câncer que, ao conhecer Augustus Waters em um grupo de apoio, desenvolve uma intensa relação enquanto juntos enfrentam os desafios da doença e exploram temas de amor, perda e a busca por significado na vida.'
 )
 
 criaCartao(
     'Literatura',
     'Eu e Esse Meu Coração',
-    'Eu e Esse Meu Coração, de Carina Rissi, é um romance contemporâneo que envolve a jovem e sonhadora Sofia, que se vê em meio a um dilema emocional ao ter que escolher entre seguir seu coração ou as expectativas da sociedade. A história é narrada com leveza e humor, enquanto Sofia enfrenta desafios em sua vida amorosa, especialmente com o enigmático e atraente Miguel. A autora aborda temas como autodescoberta, amor verdadeiro e a luta interna entre o que se deseja e o que se espera de nós. Com personagens bem desenvolvidos e diálogos envolventes, Rissi entrega uma narrativa que explora a busca pela felicidade e a importância de ouvir o próprio coração, tornando a leitura cativante e reflexiva.'
+    '"Eu e Esse Meu Coração", de Carina Rissi, é um romance contemporâneo que acompanha a jovem Sofia em seu dilema entre seguir seu coração ou atender às expectativas da sociedade, enquanto enfrenta desafios amorosos com o enigmático Miguel e explora temas de autodescoberta e a busca pela felicidade.'
 )
 
 criaCartao(
     'Cinema',
     'Nimona',
-    'Nimona é uma animação vibrante e cheia de ação, baseada na webcomic de ND Stevenson. A história segue Nimona, uma jovem shapeshifter que se torna aliada de Ballister Blackheart, um cavaleiro injustamente acusado de um crime. Juntos, eles desafiam a ordem estabelecida em um reino futurista que mistura elementos medievais com tecnologia avançada. O filme aborda temas como amizade, aceitação e a luta contra a opressão, enquanto Nimona, com sua personalidade rebelde e humor contagiante, questiona as normas sociais e o que significa ser um vilão ou um herói. Com uma animação estilizada e uma trilha sonora envolvente, "Nimona" é uma história emocionante que mistura aventura e reflexão, cativando tanto jovens quanto adultos.'
+    '"Nimona" é uma animação vibrante baseada na webcomic de ND Stevenson, que segue a jovem shapeshifter Nimona em sua aliança com o cavaleiro Ballister Blackheart, injustamente acusado de um crime, enquanto juntos desafiam a ordem em um reino futurista que mescla elementos medievais e tecnologia, explorando temas de amizade, aceitação e a complexidade de ser um vilão ou herói.'
 )
 
 criaCartao(
     'Literatura',
     'Verity',
-    'Verity, de Colleen Hoover, é um thriller psicológico que mergulha no mundo da literatura e dos segredos obscuros. A história acompanha Lowen Ashleigh, uma escritora em dificuldades que é contratada para finalizar a série de livros de Verity Crawford, uma autora famosa que sofreu um acidente e está incapacitada. Ao investigar os arquivos de Verity, Lowen descobre um manuscrito que revela verdades perturbadoras sobre a vida da autora, incluindo segredos sobre sua família e seus relacionamentos. A tensão aumenta à medida que Lowen se vê envolvida em um jogo psicológico, questionando a veracidade das palavras escritas e as motivações de Verity. Com reviravoltas surpreendentes e uma prosa envolvente, "Verity" mantém os leitores em suspense, explorando os limites da ambição, da moralidade e da verdade'
+    '"Verity", de Colleen Hoover, é um thriller psicológico que segue Lowen Ashleigh, uma escritora em dificuldades contratada para finalizar a série de livros de Verity Crawford, uma autora famosa incapacitada, e que, ao investigar seus arquivos, descobre um manuscrito revelador que expõe verdades perturbadoras sobre a vida de Verity, levando Lowen a questionar a veracidade das palavras e as motivações por trás delas em um jogo psicológico cheio de reviravoltas.'
 )
 
 criaCartao(
     'Cinema',
     'Robô Selvagem',
-    'Robô Selvagem é uma animação divertida e emocionante que segue a história de um robô desajeitado e carismático, que se torna um inesperado herói ao se unir a um grupo de crianças em uma aventura para salvar sua cidade. Com um enredo que mistura ação e comédia, o filme explora temas como amizade, coragem e a aceitação das diferenças. A animação é rica em visuais vibrantes e diálogos espirituosos, cativando tanto o público jovem quanto os adultos. A jornada do robô, que aprende a se valorizar e a encontrar seu lugar no mundo, ressoa com os espectadores, tornando "Robo Selvagem" uma história encantadora sobre a descoberta de si mesmo e a importância de lutar pelo que é certo.'
+    '"Robô Selvagem" é uma animação divertida que narra a jornada de um robô desajeitado que se torna um herói inesperado ao se unir a um grupo de crianças para salvar sua cidade, explorando temas de amizade, coragem e aceitação das diferenças, enquanto oferece visuais vibrantes e diálogos espirituosos que cativam tanto jovens quanto adultos.'
 )
 criaCartao(
     'Literatura',
     'Frankenstein',
-    'Frankenstein, de Mary Shelley, é um marco da literatura gótica que narra a trágica história de Victor Frankenstein, um cientista que, em sua busca insaciável por conhecimento, cria uma criatura a partir de partes de cadáveres. Ao se deparar com a horrenda realidade de sua criação, Victor a abandona, desencadeando uma série de eventos devastadores. A obra explora temas profundos como a natureza da humanidade, a busca por aceitação e as consequências da ambição desmedida, enquanto a criatura, rejeitada pela sociedade e seu criador, luta por amor e compreensão, mas é consumida pela dor e pelo desejo de vingança. Com sua narrativa provocativa, "Frankenstein" continua relevante, levantando questões sobre ética na ciência e a responsabilidade que vem com o ato de criar.'
+    '"Frankenstein", de Mary Shelley, é um marco da literatura gótica que relata a trágica história de Victor Frankenstein, um cientista que, em sua busca por conhecimento, cria uma criatura a partir de cadáveres e, ao abandoná-la, desencadeia eventos devastadores que exploram a natureza da humanidade, a busca por aceitação e as consequências da ambição desmedida, enquanto a criatura, rejeitada, luta por amor e compreensão, levantando questões sobre ética na ciência e a responsabilidade da criação.'
 )
